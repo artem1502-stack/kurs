bf56e6656fd2a854ff70e4e2432e8b7ad60990aa
ckarol <ckarol@student.21-school.ru>
2022-08-26 13:47:17 +0300

    1.5 | h0pe 4 success

--- t1.py
+++ t1.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numbu import njit
+from numba import njit
 
 n = 10
 
@@ -7,26 +7,43 @@ nn1 = 1.
 nn2 = 10.
 ss1 = 0.25
 ss2 = 0.2
+m = 0.1
 
 @njit
 def B(s):
 	return k1(s) / nn1 + k2(s) / nn2 
 
-
 @njit
 def k1(s):
-	return s ** 2
+	if s < ss1:
+		return 0
+	return ((s - ss1) / (1 - ss1)) ** 2
 
 @njit
 def k2(s):
-	return (1 - s) ** 2
+	if s > ss2:
+		return 0
+	return (1 - (s / ss2)) ** 2
 
 @njit
 def phi(s):
-	(k1(s) / nn1) / (k1(s) / nn1 + k2(s) / nn2)
+	return (k1(s) / nn1) / (k1(s) / nn1 + k2(s) / nn2)
 
 
-def progonka_count(a, b, c, h, p1, p2):
+def s_iter(s, p, t, h, tau):
+
+	for i in range(1, n - 1):
+		aa1 = B((s[t][i] + s[t][i + 1]) / 2) * phi((s[t][i] + s[t][i + 1]) / 2)
+		ab1 = (p[t + 1][i + 1] - p[t + 1][i]) / h
+		aa2 = B((s[t][i - 1] + s[t][i]) / 2) * phi((s[t][i - 1] + s[t][i]) / 2)
+		ab2 = (p[t + 1][i] - p[t + 1][i - 1]) / h
+		
+		print(f"aa1 = {aa1},\taa2 = {aa2}")
+		s[t + 1][i] = s[t][i] + (tau / (m * h)) * (aa1 * ab1 - aa2 * ab2)
+	s[t + 1][n - 1] = s[t][n - 1]
+	return s
+
+def progonka_count(a, b, c, p1, p2):
 	y = np.zeros(n)
 	alpha = np.zeros(n)
 	beta = np.zeros(n)
@@ -42,10 +59,10 @@ def progonka_count(a, b, c, h, p1, p2):
 	y[n - 1] = b[n - 1] + a[n - 1] * alpha[n - 2]
 	beta[n - 1] = -a[n - 1] * beta[n - 2] / y[n - 1]
 
-	x[n - 1] = p2
+	x[n - 1] = p1
 	for i in range(n - 2, -1, -1):
 		x[i] = alpha[i] * x[i + 1] + beta[i]
-	return x
+	return x[::-1]
 
 def get_abch(s, t, L):
 	a, b, c = np.zeros(n), np.zeros(n), np.zeros(n)
@@ -63,14 +80,37 @@ def get_abch(s, t, L):
 
 	return a, b, c, (L / (n - 1))
 
+def print_mass(m, s):
+	print(s)
+	for i in m:
+		for j in i:
+			print(f"{j:.2e}", end="\t")
+		print("")
+
+
 def main():
+	p1 = 1
+	p2 = 0
 	s = np.zeros((n, n))
+	p = np.zeros((n, n))
 	L = 1
-	s[:][0] = 1
+	
+	p[:, 0] = p1
+	p[:, n - 1] = p2
+	
+	s[:, 0] = 1
+	print(p)
+	print(s)
 	t = 0
 	a, b, c, h = get_abch(s, t, L)
-
-	print(s)
+	p[t] = progonka_count(a, b, c, p1, p2)
+	s = s_iter(s, p, t, h, h)
+	#for t in range(n - 1):
+	#	a, b, c, h = get_abch(s, t, L)
+	#	p[t] = progonka_count(a, b, c, p1, p2)
+	#	s = s_iter(s, p, t, h, h)
+	print_mass(p, "p mass:")
+	print_mass(s, "s mass:")
 
 if __name__ == "__main__":
 	main()
